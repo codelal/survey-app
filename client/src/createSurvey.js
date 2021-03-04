289c62c25cdc5be06efc73a20655a70d3fbe17ba
Alma <al.klein@posteo.de>
2021-03-04 16:01:12 +0100

    little styling

--- client/src/createSurvey.js
+++ client/src/createSurvey.js
@@ -6,14 +6,14 @@ import Button from "@material-ui/core/Button";
 import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
 import RemoveCircleOutlineOutlinedIcon from "@material-ui/icons/RemoveCircleOutlineOutlined";
 import { IconButton } from "@material-ui/core";
-import { styles } from "./styles";
+import { useStyles } from "./styles";
 import { theme } from "./theme";
 import Typography from "@material-ui/core/Typography";
 import { ThemeProvider } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 
 export default function CreateSurvey() {
-    const classes = styles();
+    const classes = useStyles();
     const [error, setError] = useState("");
     const [title, setTitle] = useState("");
     const [inputFields, setInputFields] = useState([{ question: "" }]);
@@ -87,50 +87,42 @@ export default function CreateSurvey() {
                     variant="standard"
                     onChange={(event) => handleTitle(event)}
                 />
-                <Grid container justify="center" direction="row">
-                    <form>
-                        {inputFields.map((inputField, index) => (
-                            <div key={index}>
-                                <Grid item xs="90%">
-                                    <Textfield
-                                        className={classes.textfield}
-                                        name="question"
-                                        placeholder={`${index + 1}.Question:`}
-                                        variant="standard"
-                                        value={inputField.question}
-                                        onChange={(event) =>
-                                            handleInput(index, event)
-                                        }
-                                    />
-                                </Grid>
-                                <Grid item xs="10%">
-                                    <IconButton
-                                        className={classes.remove}
-                                        onClick={() => removeQuestion(index)}
-                                    >
-                                        <RemoveCircleOutlineOutlinedIcon />
-                                    </IconButton>
-                                </Grid>
-                            </div>
-                        ))}
-                        <IconButton
-                            className={classes.add}
-                            onClick={() => addQuestion()}
-                        >
-                            <AddCircleOutlineOutlinedIcon />{" "}
-                        </IconButton>
+                <form className={classes.root}>
+                    {inputFields.map((inputField, index) => (
+                        <div key={index} className={classes.container}>
+                            <Textfield
+                                name="question"
+                                placeholder={`${index + 1}.Question:`}
+                                variant="standard"
+                                value={inputField.question}
+                                onChange={(event) => handleInput(index, event)}
+                            />
 
-                        <Button
-                            className={classes.button}
-                            variant="contained"
-                            type="submit"
-                            color="primary"
-                            onClick={() => submitInput(event)}
-                        >
-                            Save and publish
-                        </Button>
-                    </form>
-                </Grid>
+                            <IconButton
+                                className={classes.remove}
+                                onClick={() => removeQuestion(index)}
+                            >
+                                <RemoveCircleOutlineOutlinedIcon />
+                            </IconButton>
+                        </div>
+                    ))}
+                    <IconButton
+                        className={classes.add}
+                        onClick={() => addQuestion()}
+                    >
+                        <AddCircleOutlineOutlinedIcon />{" "}
+                    </IconButton>
+
+                    <Button
+                        className={classes.button}
+                        variant="contained"
+                        type="submit"
+                        color="primary"
+                        onClick={() => submitInput(event)}
+                    >
+                        Save and publish
+                    </Button>
+                </form>
             </ThemeProvider>
         </Container>
     );
--- client/src/styles.js
+++ client/src/styles.js
@@ -1,22 +1,59 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export const styles = makeStyles(() => ({
-    labelRoot: {
-        color: "red",
+export const useStyles = makeStyles(() => ({
+    root: {
+        "& .MuiInputBase-root": {
+            width: "200%",
+            marginTop: 30,
+            paddingLeft: 0,
+        },
+    },
+    container: {
+        display: "flex",
+        justifyContent: "center",
+        padding: 0,
+        border: 1,
+        borderColor: "red",
+        borderStyle: "solid",
     },
     title: {
         marginTop: 40,
         width: "100%",
+        border: 1,
+        borderColor: "black",
+        borderStyle: "solid",
+        display: "flex",
+        justifyContent: "center",
     },
     textfield: {
         marginTop: 30,
-        width: "100%",
+        width: 200,
     },
 
     add: {
         color: "green",
+        width: "10%",
     },
     remove: {
         color: "red",
+        width: "10%",
     },
 }));
+
+// export const styles = makeStyles((theme) => ({
+//     textfield: {
+//         "& .MuiTextField-root": {
+//             width: "100%",
+//         },
+//     },
+//     add: {
+//         color: "green",
+//     },
+//     remove: {
+//         color: "red",
+//     },
+//     button: {
+//         margin: theme.spacing(3),
+//         width: "15%",
+//     },
+// }));
